0705bb800087607655a1a2f7a76d9ea8e0d99870
Javiya Deep <patelaleis@gmail.com>
2022-01-15 23:21:51 +0530

    .

---
+++ dist/js/custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $(".code-btn1").click(function () {
+    $(".code-view1").toggle(600);
+    $(".code-view1").css("display", "block");
+  });
+
+  $(".code-btn2").click(function () {
+    $(".code-view2").toggle(600);
+    $(".code-view2").css("display", "block");
+  });
+
+  $(".code-btn3").click(function () {
+    $(".code-view3").toggle(600);
+    $(".code-view3").css("display", "block");
+  });
+
+  $(".code-btn4").click(function () {
+    $(".code-view4").toggle(600);
+    $(".code-view4").css("display", "block");
+  });
+});
